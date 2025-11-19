ed1a7a297529c0d188c7724c6b8f10b1764b6094
Sahil Singh <akasahilsingh@gmail.com>
2025-11-19 23:33:36 +0530

    added Header.jsx component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
+import Header from '../component/Header'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 }
